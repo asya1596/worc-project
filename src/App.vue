16c340becc35a097ce318638baabeaf65ad064e6
asya1596 <asya15111996@gmail.com>
2025-12-20 23:02:29 +0300

    решение конфликта фалов при сохранении в репозитории локальном и удаленном

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <header-component />
+    <router-view />
+  </div>
+</template>
+
+<script lang="js" >
+import Header from './components/primari/Header.vue';
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: ' !'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "/src/assets/css/index.css";
+@import "/src/assets/sass/index.scss";
+// Здесь ваши глобальные стили (пока пустые)
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <header-component />
+  </div>
+</template>
+
+<script setup>
+import HeaderComponent from '@/components/primari/Header.vue';
+
+</script>
+
+<style lang="scss" scoped></style>
