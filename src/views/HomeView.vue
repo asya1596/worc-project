4e3a1fcc2ec3dc752811a2a004bef3466c487b86
asya1596 <asya15111996@gmail.com>
2025-12-20 12:50:59 +0300

    Создан новй проект. Удалены системные компоненты и файлы, не нужные в текущемпроекте

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h1>Главная страница</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView'
+}
+</script>
+
+<style lang="scss" scoped>
+/* Ваши стили */
+</style>
